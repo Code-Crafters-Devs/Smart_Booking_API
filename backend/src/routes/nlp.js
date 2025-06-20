2864d4993831537608ead171c483f0fa25d2cf1c
Luyanda Xhakaza <lloyiswa2003@gmail.com>
2025-06-20 15:09:30 +0200

    Add .gitkeep to all empty folders

--- backend/src/routes/nlp.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const nlpController = require('../controllers/nlpController');
-
-// Define routes for natural language processing tasks
-router.post('/analyze', nlpController.analyzeInput);
-router.post('/generate-response', nlpController.generateResponse);
-
-module.exports = router;
