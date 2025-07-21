ab019ca9f25da43378a2841ac1805f2195fcfee8
Luyanda Xhakaza <lloyiswa2003@gmail.com>
2025-07-21 08:33:01 +0200

    feat: added models for guest and administrator

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,14 +1,47 @@
-const { DataTypes } = require('sequelize');
-
-module.exports = (sequelize) => {
-  return sequelize.define('User', {
-    first_name: { type: DataTypes.STRING, allowNull: false },
-    last_name: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, unique: true, allowNull: false },
-    password_hash: { type: DataTypes.STRING, allowNull: false },
-    role_id: { type: DataTypes.INTEGER, allowNull: false },
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    first_name: { type: DataTypes.STRING(100), allowNull: false },
+    last_name: { type: DataTypes.STRING(100), allowNull: false },
+    email: { 
+      type: DataTypes.STRING(255), 
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password_hash: { type: DataTypes.STRING(255), allowNull: false },
+    role_id: { 
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Roles',
+        key: 'id'
+      }
+    }
   }, {
     timestamps: true,
-    underscored: true
+    underscored: true,
+    defaultScope: {
+      attributes: { exclude: ['password_hash'] }
+    }
   });
+
+  User.associate = (models) => {
+    User.belongsTo(models.Role, { foreignKey: 'role_id' });
+    User.hasOne(models.Provider, { 
+      foreignKey: 'user_id',
+      as: 'provider'
+    });
+    User.hasOne(models.Guest, { 
+      foreignKey: 'user_id',
+      as: 'guest'
+    });
+    User.hasOne(models.Administrator, { 
+      foreignKey: 'user_id',
+      as: 'administrator'
+    });
+  };
+
+  return User;
 };
